207531344d6cb2cae06b3b79438c518c37286cf0
leonard-creater <smileme12369@gmail.com>
2024-11-05 11:06:27 +0000

    calc

---
+++ 11/4/2024/calc.py
@@ -0,0 +1,16 @@
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+def mul(x,y):
+    return x*y
+def div(x,y):
+    return x/y
+
+num1=int(input("enter nnumber 1:"))
+num2=int(input("enter nnumber 1:"))
+
+print(add(num1,num2))
+print(sub(num1,num2))
+print(mul(num1,num2))
+print(div(num1,num2))
